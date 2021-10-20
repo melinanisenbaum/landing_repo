d06db7000cdf86e19de6390a42216e447ccb7486
melinanisenbaum <70273255+melinanisenbaum@users.noreply.github.com>
2021-10-20 15:28:29 -0300

    20/10/21last

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,23 +4,28 @@ form.addEventListener('submit', formValidation);
 
 function formValidation(event) {
     event.preventDefault();
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
 
-    let errorName = document.getElementById('error-name');
-    let errorEmail = document.getElementById('error-email');
-    let errorMsg = document.getElementById('error-msg');
+    const errorName = document.getElementById('error-name');
+    const errorEmail = document.getElementById('error-email');
+    const errorMsg = document.getElementById('error-msg');
 
     if (name.length == 0) {
-        errorName.classList.remove = 'error';
+        errorName.style.display = 'block';
+        return false;
     }
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
-        errorEmail.classList.remove = 'error';
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email) || email.length == 0) {
+        errorEmail.style.display = 'block';
+        return false;
     }
-    if (message == null || message.length == 0 || /^\s+$/.test(message) ) {
-        errorMsg.classList.remove = 'error';
+    if (message == null || message.length == 0 || /^\s+$/.test(message)) {
+        errorMsg.style.display = 'block';
+        return false;
     }
-
-    this.submit();
+    else {
+        this.submit();
+    }
 }
